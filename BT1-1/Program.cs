4a07a6081d7f00a424c22c5dda1792fb8138d61e
datkhodo888 <datkhodo888@gmail.com>
2023-09-01 12:04:14 +0700

    chinh sua lan 1

--- BT1-1/Program.cs
+++ BT1-1/Program.cs
@@ -2,43 +2,28 @@
 using System.Collections.Generic;
 using System.Linq;
 
-class Vehicle
-{
-    public string LicensePlate { get; set; }
-    public DateTime ManufacturingDate { get; set; }
-}
-
-class Car : Vehicle
-{
-    public int NumSeats { get; set; }
-    public bool IsTransportationBusiness { get; set; }
-}
-
-class Truck : Vehicle
-{
-    public double LoadCapacity { get; set; }
-}
-
 class Program
 {
-    static List<Vehicle> vehicles = new List<Vehicle>();
+    static List<Car> cars = new List<Car>();
+    static List<Truck> trucks = new List<Truck>();
 
     static void Main(string[] args)
     {
         while (true)
         {
-            Console.WriteLine("Menu:");
-            Console.WriteLine("1. Add Car");
-            Console.WriteLine("2. Add Truck");
-            Console.WriteLine("3. Display All Vehicles");
-            Console.WriteLine("4. Find Car with Most Seats");
-            Console.WriteLine("5. Sort Trucks by Load Capacity");
-            Console.WriteLine("6. Display Beautiful License Plates");
-            Console.WriteLine("7. Calculate Inspection Fees");
-            Console.WriteLine("8. Calculate Upcoming Inspection Dates");
-            Console.WriteLine("9. Calculate Total Inspection Fees");
-            Console.WriteLine("0. Exit");
+            Console.WriteLine("===== Quản lý xe =====");
+            Console.WriteLine("1. Them xe o to");
+            Console.WriteLine("2. Them xe tai");
+            Console.WriteLine("3. Xuat danh sach xe");
+            Console.WriteLine("4. Tim xe o to co cho ngoi nhieu nhat");
+            Console.WriteLine("5. Sap xep danh sach xe tai theo trong tai");
+            Console.WriteLine("6. Xuat danh sach cac bien so xe dep");
+            Console.WriteLine("7. Tinh so tien dang kiem dinh ky cua tung xe");
+            Console.WriteLine("8. Tinh thoi gian dang kiem dinh ky cua tung xe sap toi");
+            Console.WriteLine("9. Tinh tong so tien da dang kiem");
+            Console.WriteLine("0. Thoat");
 
+            Console.Write("Nhap lua chon cua ban: ");
             int choice = int.Parse(Console.ReadLine());
 
             switch (choice)
@@ -56,16 +41,16 @@ class Program
                     FindCarWithMostSeats();
                     break;
                 case 5:
-                    SortTrucksByLoadCapacity();
+                    SortTrucksByPayload();
                     break;
                 case 6:
                     DisplayBeautifulLicensePlates();
                     break;
                 case 7:
-                    CalculateInspectionFees();
+                    CalculateInspectionFee();
                     break;
                 case 8:
-                    CalculateUpcomingInspectionDates();
+                    CalculateNextInspectionDate();
                     break;
                 case 9:
                     CalculateTotalInspectionFees();
@@ -74,7 +59,7 @@ class Program
                     Environment.Exit(0);
                     break;
                 default:
-                    Console.WriteLine("Invalid choice. Please choose again.");
+                    Console.WriteLine("Lua chon khong hop le,vui long ch.");
                     break;
             }
         }
@@ -82,214 +67,268 @@ class Program
 
     static void AddCar()
     {
-        Console.Write("Enter license plate: ");
+        Console.Write("Nhap ngay san xuat (yyyy-MM-dd): ");
+        DateTime manufactureDate = DateTime.Parse(Console.ReadLine());
+        Console.Write("Nhap bien so xe (vd: 62B6-67519): ");
         string licensePlate = Console.ReadLine();
-        Console.Write("Enter manufacturing date (yyyy-mm-dd): ");
-        DateTime manufacturingDate = DateTime.Parse(Console.ReadLine());
-        Console.Write("Enter number of seats: ");
-        int numSeats = int.Parse(Console.ReadLine());
-        Console.Write("Is for transportation business? (true/false): ");
-        bool isTransportationBusiness = bool.Parse(Console.ReadLine());
+        Console.Write("Nhap so cho ngoi: ");
+        int seats = int.Parse(Console.ReadLine());
+        Console.Write("Co dang ky kinh doanh van tai (true/false): ");
+        bool isBusiness = bool.Parse(Console.ReadLine());
 
-        Car car = new Car
-        {
-            LicensePlate = licensePlate,
-            ManufacturingDate = manufacturingDate,
-            NumSeats = numSeats,
-            IsTransportationBusiness = isTransportationBusiness
-        };
-
-        vehicles.Add(car);
-        Console.WriteLine("Car added successfully.");
+        Car car = new Car(manufactureDate, licensePlate, seats, isBusiness);
+        cars.Add(car);
+        Console.WriteLine("Them xe o to thanh cong!");
     }
 
     static void AddTruck()
     {
-        Console.Write("Enter license plate: ");
+        Console.Write("Nhap ngay san xuat (yyyy-MM-dd): ");
+        DateTime manufactureDate = DateTime.Parse(Console.ReadLine());
+        Console.Write("Nhap bien so xe (vd: 30C3-12345): ");
         string licensePlate = Console.ReadLine();
-        Console.Write("Enter manufacturing date (yyyy-mm-dd): ");
-        DateTime manufacturingDate = DateTime.Parse(Console.ReadLine());
-        Console.Write("Enter load capacity (tons): ");
-        double loadCapacity = double.Parse(Console.ReadLine());
+        Console.Write("Nhap trong tai (tấn): ");
+        double payload = double.Parse(Console.ReadLine());
 
-        Truck truck = new Truck
-        {
-            LicensePlate = licensePlate,
-            ManufacturingDate = manufacturingDate,
-            LoadCapacity = loadCapacity
-        };
-
-        vehicles.Add(truck);
-        Console.WriteLine("Truck added successfully.");
+        Truck truck = new Truck(manufactureDate, licensePlate, payload);
+        trucks.Add(truck);
+        Console.WriteLine("Them xe tai thanh cong!");
     }
 
     static void DisplayAllVehicles()
     {
-        Console.WriteLine("List of all vehicles:");
-        foreach (var vehicle in vehicles)
+        Console.WriteLine("===== Danh sach xe o to =====");
+        foreach (var car in cars)
         {
-            Console.WriteLine($"License Plate: {vehicle.LicensePlate}, Manufacturing Date: {vehicle.ManufacturingDate}");
+            Console.WriteLine(car.ToString());
+        }
+
+        Console.WriteLine("===== Danh sach xe tai =====");
+        foreach (var truck in trucks)
+        {
+            Console.WriteLine(truck.ToString());
         }
     }
 
     static void FindCarWithMostSeats()
     {
-        var cars = vehicles.OfType<Car>();
-        if (cars.Any())
+        var carWithMostSeats = cars.OrderByDescending(c => c.Seats).FirstOrDefault();
+        if (carWithMostSeats != null)
         {
-            var carWithMostSeats = cars.OrderByDescending(car => car.NumSeats).FirstOrDefault();
-            Console.WriteLine($"Car with most seats: License Plate: {carWithMostSeats.LicensePlate}, Number of Seats: {carWithMostSeats.NumSeats}");
+            Console.WriteLine($"Xe o to co so cho ngoi nhieu nhat: {carWithMostSeats.ToString()}");
         }
         else
         {
-            Console.WriteLine("No cars found.");
+            Console.WriteLine("Khong co xe o to nao trong danh sach.");
         }
     }
 
-    static void SortTrucksByLoadCapacity()
+    static void SortTrucksByPayload()
     {
-        var trucks = vehicles.OfType<Truck>();
-        if (trucks.Any())
+        var sortedTrucks = trucks.OrderBy(t => t.Payload).ToList();
+        Console.WriteLine("===== Danh sach xe tai sap xep theo trong tai tang dan =====");
+        foreach (var truck in sortedTrucks)
         {
-            var sortedTrucks = trucks.OrderBy(truck => truck.LoadCapacity);
-            Console.WriteLine("Trucks sorted by load capacity:");
-            foreach (var truck in sortedTrucks)
-            {
-                Console.WriteLine($"License Plate: {truck.LicensePlate}, Load Capacity: {truck.LoadCapacity} tons");
-            }
-        }
-        else
-        {
-            Console.WriteLine("No trucks found.");
+            Console.WriteLine(truck.ToString());
         }
     }
 
     static void DisplayBeautifulLicensePlates()
     {
-        var beautifulPlates = vehicles.Where(vehicle => IsBeautifulLicensePlate(vehicle.LicensePlate));
-        if (beautifulPlates.Any())
+        var beautifulLicensePlates = cars.Where(c => IsBeautifulLicensePlate(c.LicensePlate))
+                                          .Select(c => c.LicensePlate)
+                                          .ToList();
+        Console.WriteLine("===== Danh sach cac bien so xe đep =====");
+        foreach (var licensePlate in beautifulLicensePlates)
         {
-            Console.WriteLine("Beautiful license plates:");
-            foreach (var vehicle in beautifulPlates)
-            {
-                Console.WriteLine($"License Plate: {vehicle.LicensePlate}");
-            }
+            Console.WriteLine(licensePlate);
+        }
+    }
+
+    static void CalculateInspectionFee()
+    {
+        double totalFee = 0;
+
+        foreach (var car in cars)
+        {
+            int inspectionInterval = car.CalculateInspectionInterval();
+            double inspectionFee = car.CalculateInspectionFee();
+
+            totalFee += inspectionFee;
+
+            Console.WriteLine($"Bien so: {car.LicensePlate}, Thoi gian đang kiem: {inspectionInterval} thang, Phi đang kiem: {inspectionFee} VND");
+        }
+
+        foreach (var truck in trucks)
+        {
+            int inspectionInterval = truck.CalculateInspectionInterval();
+            double inspectionFee = truck.CalculateInspectionFee();
+
+            totalFee += inspectionFee;
+
+            Console.WriteLine($"Bien so: {truck.LicensePlate}, Thoi gian đang kiem: {inspectionInterval} thang, Phi đang kiem: {inspectionFee} VND");
+        }
+
+        Console.WriteLine($"Tong phi đang kiem: {totalFee} VND");
+    }
+
+    static void CalculateNextInspectionDate()
+    {
+        Console.Write("Nhap bien so xe: ");
+        string licensePlate = Console.ReadLine();
+
+        var car = cars.FirstOrDefault(c => c.LicensePlate == licensePlate);
+        var truck = trucks.FirstOrDefault(t => t.LicensePlate == licensePlate);
+
+        if (car != null)
+        {
+            int nextInspectionInterval = car.CalculateInspectionInterval();
+            DateTime nextInspectionDate = DateTime.Now.AddMonths(nextInspectionInterval);
+
+            Console.WriteLine($"Xe o to co bien so {licensePlate}, Thoi gian đang kiem tiep theo: {nextInspectionDate.ToString("yyyy-MM-dd")}");
+        }
+        else if (truck != null)
+        {
+            int nextInspectionInterval = truck.CalculateInspectionInterval();
+            DateTime nextInspectionDate = DateTime.Now.AddMonths(nextInspectionInterval);
+
+            Console.WriteLine($"Xe tai co bien so {licensePlate}, Thoi gian đang kiem tiep theo: {nextInspectionDate.ToString("yyyy-MM-dd")}");
         }
         else
         {
-            Console.WriteLine("No beautiful license plates found.");
+            Console.WriteLine("Khong tim thay xe voi bien so đa nhap.");
         }
-    }
-
-    static bool IsBeautifulLicensePlate(string licensePlate)
-    {
-        string[] parts = licensePlate.Split('-');
-        string lastPart = parts[1];
-        return lastPart.Distinct().Count() >= 4;
-    }
-
-    static void CalculateInspectionFees()
-    {
-        double totalFees = 0;
-        foreach (var vehicle in vehicles)
-        {
-            double fee = CalculateVehicleInspectionFee(vehicle);
-            totalFees += fee;
-            Console.WriteLine($"License Plate: {vehicle.LicensePlate}, Inspection Fee: {fee} VND");
-        }
-        Console.WriteLine($"Total Inspection Fees: {totalFees} VND");
-    }
-
-    static double CalculateVehicleInspectionFee(Vehicle vehicle)
-    {
-        if (vehicle is Car car)
-        {
-            if (DateTime.Now.Year - car.ManufacturingDate.Year <= 7)
-            {
-                if (car.NumSeats <= 10)
-                {
-                    return car.IsTransportationBusiness ? 320000 : 240000;
-                }
-                else
-                {
-                    return 320000;
-                }
-            }
-            else
-            {
-                return 60000;
-            }
-        }
-        else if (vehicle is Truck truck)
-        {
-            if (DateTime.Now.Year - truck.ManufacturingDate.Year <= 20)
-            {
-                if (truck.LoadCapacity > 20)
-                {
-                    return 560000;
-                }
-                else if (truck.LoadCapacity >= 7 && truck.LoadCapacity <= 20)
-                {
-                    return 350000;
-                }
-                else
-                {
-                    return 320000;
-                }
-            }
-            else
-            {
-                return 30000;
-            }
-        }
-        return 0;
-    }
-
-    static void CalculateUpcomingInspectionDates()
-    {
-        foreach (var vehicle in vehicles)
-        {
-            DateTime nextInspectionDate = CalculateNextInspectionDate(vehicle);
-            Console.WriteLine($"License Plate: {vehicle.LicensePlate}, Next Inspection Date: {nextInspectionDate.ToString("yyyy-MM-dd")}");
-        }
-    }
-
-    static DateTime CalculateNextInspectionDate(Vehicle vehicle)
-    {
-        int inspectionIntervalMonths;
-        if (vehicle is Car car)
-        {
-            if (DateTime.Now.Year - car.ManufacturingDate.Year <= 7)
-            {
-                inspectionIntervalMonths = car.NumSeats <= 10 && car.IsTransportationBusiness ? 12 : 24;
-            }
-            else
-            {
-                inspectionIntervalMonths = 6;
-            }
-        }
-        else if (vehicle is Truck truck)
-        {
-            if (DateTime.Now.Year - truck.ManufacturingDate.Year <= 20)
-            {
-                inspectionIntervalMonths = truck.LoadCapacity > 20 ? 3 : 6;
-            }
-            else
-            {
-                inspectionIntervalMonths = 3;
-            }
-        }
-        else
-        {
-            inspectionIntervalMonths = 0;
-        }
-
-        return vehicle.ManufacturingDate.AddMonths(inspectionIntervalMonths);
     }
 
     static void CalculateTotalInspectionFees()
     {
-        double totalFees = vehicles.Sum(vehicle => CalculateVehicleInspectionFee(vehicle));
-        Console.WriteLine($"Total Inspection Fees: {totalFees} VND");
+        double totalFee = 0;
+
+        foreach (var car in cars)
+        {
+            double inspectionFee = car.CalculateInspectionFee();
+            totalFee += inspectionFee;
+        }
+
+        foreach (var truck in trucks)
+        {
+            double inspectionFee = truck.CalculateInspectionFee();
+            totalFee += inspectionFee;
+        }
+
+        Console.WriteLine($"Tong so tien đa đang kiem: {totalFee} VND");
+    }
+
+    static bool IsBeautifulLicensePlate(string licensePlate)
+    {
+        string lastFiveDigits = licensePlate.Substring(6, 5);
+        return lastFiveDigits.Distinct().Count() <= 2;
+    }
+}
+
+class Vehicle
+{
+    public DateTime ManufactureDate { get; }
+    public string LicensePlate { get; }
+
+    public Vehicle(DateTime manufactureDate, string licensePlate)
+    {
+        ManufactureDate = manufactureDate;
+        LicensePlate = licensePlate;
+    }
+
+    public virtual int CalculateInspectionInterval()
+    {
+        return 6;
+    }
+
+    public override string ToString()
+    {
+        return $"Bien so: {LicensePlate}, Ngay san xuat: {ManufactureDate.ToString("yyyy-MM-dd")}";
+    }
+}
+
+class Car : Vehicle
+{
+    public int Seats { get; }
+    public bool IsBusiness { get; }
+
+    public Car(DateTime manufactureDate, string licensePlate, int seats, bool isBusiness)
+        : base(manufactureDate, licensePlate)
+    {
+        Seats = seats;
+        IsBusiness = isBusiness;
+    }
+
+    public override int CalculateInspectionInterval()
+    {
+        int yearsSinceManufacture = DateTime.Now.Year - ManufactureDate.Year;
+        int inspectionInterval = 6;  // Default interval for cars over 7 years
+
+        if (yearsSinceManufacture <= 7)
+        {
+            if (Seats <= 10)
+            {
+                inspectionInterval = IsBusiness ? 12 : 24;
+            }
+            else
+            {
+                inspectionInterval = 12;
+            }
+        }
+
+        return inspectionInterval;
+    }
+
+    public double CalculateInspectionFee()
+    {
+        double fee = Seats <= 10 ? 240000 : 320000;
+        return fee;
+    }
+
+    public override string ToString()
+    {
+        return base.ToString() + $", So cho: {Seats}, Đang ky kinh doanh: {IsBusiness}";
+    }
+}
+
+class Truck : Vehicle
+{
+    public double Payload { get; }
+
+    public Truck(DateTime manufactureDate, string licensePlate, double payload)
+        : base(manufactureDate, licensePlate)
+    {
+        Payload = payload;
+    }
+
+    public override int CalculateInspectionInterval()
+    {
+        int yearsSinceManufacture = DateTime.Now.Year - ManufactureDate.Year;
+        int inspectionInterval = yearsSinceManufacture <= 20 ? 6 : 3;
+        return inspectionInterval;
+    }
+
+    public double CalculateInspectionFee()
+    {
+        double fee;
+        if (Payload > 20)
+        {
+            fee = 560000;
+        }
+        else if (Payload >= 7)
+        {
+            fee = 350000;
+        }
+        else
+        {
+            fee = 320000;
+        }
+        return fee;
+    }
+
+    public override string ToString()
+    {
+        return base.ToString() + $", Trong tai: {Payload} tan";
     }
 }
